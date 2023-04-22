2f7c4e45893c0ee1c7001727ea1914e737e57c14
SPal-14 <kolkatasouvik1@gmail.com>
2023-04-23 00:26:57 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,168 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
-
 function App() {
+  const [survey, setSurvey] = useState(null);
+  const [error, setError] = useState(null);
+  const [fileInput, setFileInput] = useState(null);
+  const [responses, setResponses] = useState({});
+
+  function handleInputChange(e) {
+    try {
+      const inputJson = JSON.parse(e.target.value);
+      if (Array.isArray(inputJson) && inputJson.length > 0) {
+        setSurvey(inputJson[0]);
+        setError(null);
+      } else {
+        setError("Input must be an array of survey questions");
+      }
+    } catch (e) {
+      setError("Invalid JSON format");
+    }
+  }
+
+  function handleFileChange(e) {
+    const fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      try {
+        const inputJson = JSON.parse(e.target.result);
+        if (Array.isArray(inputJson) && inputJson.length > 0) {
+          setSurvey(inputJson[0]);
+          setError(null);
+        } else {
+          setError("Input must be an array of survey questions");
+        }
+      } catch (e) {
+        setError("Invalid JSON format");
+      }
+    };
+    fileReader.readAsText(e.target.files[0]);
+    setFileInput(e.target.value);
+  }
+  function handleResponse(question, response) {
+    setResponses((prevState) => ({
+      ...prevState,
+      [question.pollid]: response,
+    }));
+  }
+
+  function renderQuestion(question) {
+    switch (question.pollType) {
+      case 0: // No response/statement only
+        return (
+          <div key={question.pollid}>
+            <h3>{question.questionDisplay}</h3>
+            <p>No response required.</p>
+          </div>
+        );
+      case 1: // Free text
+        return (
+          <div key={question.pollid}>
+            <h3>{question.questionDisplay}</h3>
+            <input type="text" onChange={(e) => handleResponse(question, e.target.value)} />
+          </div>
+        );
+      case 2: // Numeric
+        return (
+          <div key={question.pollid}>
+            <h3>{question.questionDisplay}</h3>
+            <input type="number" onChange={(e) => handleResponse(question, e.target.value)} />
+          </div>
+        );
+      case 3: // Binary
+        return (
+          <div key={question.pollid}>
+            <h3>{question.questionDisplay}</h3>
+            <div>
+              <label>
+              <input type="radio" name={question.pollid} value="Yes" onChange={() => handleResponse(question, "Yes")} />
+                Yes
+              </label>
+            </div>
+            <div>
+              <label>
+              <input type="radio" name={question.pollid} value="No" onChange={() => handleResponse(question, "No")} />
+                No
+              </label>
+            </div>
+          </div>
+        );
+      case 4: // Multiple choice single choice
+        return (
+          <div key={question.pollid}>
+            <h3>{question.questionDisplay}</h3>
+            {question.pollOptions.map((option) => (
+              <div key={option.optionId}>
+                <label>
+                <input type="radio" name={question.pollid} value={option} onChange={() => handleResponse(question, option)} />
+                  {option.option}
+                </label>
+              </div>
+            ))}
+          </div>
+        );
+      case 5: // Multiple choice multi choice
+        return (
+          <div key={question.pollid}>
+            <h3>{question.questionDisplay}</h3>
+            {question.pollOptions.map((option) => (
+              <div key={option.optionId}>
+                <label>
+                <input type="checkbox" name={question.pollid} value={option} onChange={() => handleResponse(question, option)} />
+                  {option.option}
+                </label>
+              </div>
+            ))}
+          </div>
+        );
+      case 6: // Voice STT and TTS
+        return (
+          <div key={question.pollid}>
+            <h3>{question.questionDisplay}</h3>
+            <button>Record</button>
+            <button>Stop</button>
+            <button>Playback</button>
+          </div>
+        );
+      default:
+        return null; // Unsupported question type
+    }
+  }
+ 
+
+  function handleDownload() {
+    const data = {
+      survey: survey,
+      responses: responses,
+    };
+    const fileName = "survey_responses.json";
+    const contentType = "application/json;charset=utf-8;";
+    const blob = new Blob([JSON.stringify(data)], { type: contentType });
+    if (navigator.msSaveBlob) {
+      navigator.msSaveBlob(blob, fileName);
+    } else {
+      const downloadLink = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = downloadLink;
+      a.download = fileName;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  }
+
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{survey ? survey.title : "Dynamic Survey"}</h1>
+      <p>{survey ? survey.referenceId : "Enter your survey JSON below or upload a file"}</p>
+      <textarea rows="10" onChange={handleInputChange} value={fileInput} />
+      <input type="file" accept=".json" onChange={handleFileChange} />
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {survey &&
+        survey.pollItems.map((question) => renderQuestion(question))}
+        <button onClick={handleDownload}>Download Responses</button>
     </div>
   );
 }
